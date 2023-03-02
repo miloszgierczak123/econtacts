679ef5147ea33bcedb6090e93c23aeedf8b0e9ea
Miłosz <miloszgierczak123@gmail.com>
2023-03-02 14:33:27 +0100

    test

---
+++ EContact/Econtact.Designer.cs
@@ -0,0 +1,334 @@
+﻿namespace EContact
+{
+    partial class Econtact
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Econtact));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblIDContact = new System.Windows.Forms.Label();
+            this.lblFirstName = new System.Windows.Forms.Label();
+            this.lblLastName = new System.Windows.Forms.Label();
+            this.lblContactNo = new System.Windows.Forms.Label();
+            this.lblGender = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textFirstName = new System.Windows.Forms.TextBox();
+            this.textLastName = new System.Windows.Forms.TextBox();
+            this.textContactNo = new System.Windows.Forms.TextBox();
+            this.lblAddress = new System.Windows.Forms.Label();
+            this.textAddress = new System.Windows.Forms.TextBox();
+            this.comboBoxGender = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.dgvContactList = new System.Windows.Forms.DataGridView();
+            this.lblSearch = new System.Windows.Forms.Label();
+            this.textBoxSearch = new System.Windows.Forms.TextBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvContactList)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(222, -19);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(490, 245);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // lblIDContact
+            // 
+            this.lblIDContact.AutoSize = true;
+            this.lblIDContact.BackColor = System.Drawing.Color.Transparent;
+            this.lblIDContact.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblIDContact.Location = new System.Drawing.Point(44, 226);
+            this.lblIDContact.Name = "lblIDContact";
+            this.lblIDContact.Size = new System.Drawing.Size(106, 22);
+            this.lblIDContact.TabIndex = 1;
+            this.lblIDContact.Text = "Contact ID";
+            this.lblIDContact.Click += new System.EventHandler(this.lblIDContact_Click);
+            // 
+            // lblFirstName
+            // 
+            this.lblFirstName.AutoSize = true;
+            this.lblFirstName.BackColor = System.Drawing.Color.Transparent;
+            this.lblFirstName.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblFirstName.Location = new System.Drawing.Point(44, 252);
+            this.lblFirstName.Name = "lblFirstName";
+            this.lblFirstName.Size = new System.Drawing.Size(109, 22);
+            this.lblFirstName.TabIndex = 2;
+            this.lblFirstName.Text = "First Name";
+            // 
+            // lblLastName
+            // 
+            this.lblLastName.AutoSize = true;
+            this.lblLastName.BackColor = System.Drawing.Color.Transparent;
+            this.lblLastName.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblLastName.Location = new System.Drawing.Point(45, 278);
+            this.lblLastName.Name = "lblLastName";
+            this.lblLastName.Size = new System.Drawing.Size(107, 22);
+            this.lblLastName.TabIndex = 3;
+            this.lblLastName.Text = "Last Name";
+            this.lblLastName.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // lblContactNo
+            // 
+            this.lblContactNo.AutoSize = true;
+            this.lblContactNo.BackColor = System.Drawing.Color.Transparent;
+            this.lblContactNo.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblContactNo.Location = new System.Drawing.Point(45, 304);
+            this.lblContactNo.Name = "lblContactNo";
+            this.lblContactNo.Size = new System.Drawing.Size(117, 22);
+            this.lblContactNo.TabIndex = 4;
+            this.lblContactNo.Text = "Contact No.";
+            this.lblContactNo.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // lblGender
+            // 
+            this.lblGender.AutoSize = true;
+            this.lblGender.BackColor = System.Drawing.Color.Transparent;
+            this.lblGender.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblGender.Location = new System.Drawing.Point(44, 470);
+            this.lblGender.Name = "lblGender";
+            this.lblGender.Size = new System.Drawing.Size(79, 22);
+            this.lblGender.TabIndex = 5;
+            this.lblGender.Text = "Gender";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(222, 226);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(184, 20);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textFirstName
+            // 
+            this.textFirstName.Location = new System.Drawing.Point(222, 252);
+            this.textFirstName.Name = "textFirstName";
+            this.textFirstName.Size = new System.Drawing.Size(184, 20);
+            this.textFirstName.TabIndex = 7;
+            this.textFirstName.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textLastName
+            // 
+            this.textLastName.Location = new System.Drawing.Point(222, 278);
+            this.textLastName.Name = "textLastName";
+            this.textLastName.Size = new System.Drawing.Size(184, 20);
+            this.textLastName.TabIndex = 8;
+            this.textLastName.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // textContactNo
+            // 
+            this.textContactNo.Location = new System.Drawing.Point(222, 304);
+            this.textContactNo.Name = "textContactNo";
+            this.textContactNo.Size = new System.Drawing.Size(184, 20);
+            this.textContactNo.TabIndex = 9;
+            this.textContactNo.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // lblAddress
+            // 
+            this.lblAddress.AutoSize = true;
+            this.lblAddress.BackColor = System.Drawing.Color.Transparent;
+            this.lblAddress.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblAddress.Location = new System.Drawing.Point(45, 330);
+            this.lblAddress.Name = "lblAddress";
+            this.lblAddress.Size = new System.Drawing.Size(88, 22);
+            this.lblAddress.TabIndex = 10;
+            this.lblAddress.Text = "Address";
+            this.lblAddress.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textAddress
+            // 
+            this.textAddress.Location = new System.Drawing.Point(222, 330);
+            this.textAddress.Multiline = true;
+            this.textAddress.Name = "textAddress";
+            this.textAddress.Size = new System.Drawing.Size(184, 133);
+            this.textAddress.TabIndex = 11;
+            // 
+            // comboBoxGender
+            // 
+            this.comboBoxGender.FormattingEnabled = true;
+            this.comboBoxGender.Items.AddRange(new object[] {
+            "Male",
+            "Female"});
+            this.comboBoxGender.Location = new System.Drawing.Point(222, 473);
+            this.comboBoxGender.Name = "comboBoxGender";
+            this.comboBoxGender.Size = new System.Drawing.Size(184, 21);
+            this.comboBoxGender.TabIndex = 12;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(222, 509);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 32);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Add";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(351, 509);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(103, 32);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "Update";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(480, 509);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(103, 32);
+            this.button3.TabIndex = 15;
+            this.button3.Text = "Delete";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(609, 509);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(103, 32);
+            this.button4.TabIndex = 16;
+            this.button4.Text = "Clear";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // dgvContactList
+            // 
+            this.dgvContactList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvContactList.Location = new System.Drawing.Point(428, 252);
+            this.dgvContactList.Name = "dgvContactList";
+            this.dgvContactList.Size = new System.Drawing.Size(464, 242);
+            this.dgvContactList.TabIndex = 17;
+            this.dgvContactList.RowHeaderMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dgvContactList_RowHeaderMouseClick);
+            // 
+            // lblSearch
+            // 
+            this.lblSearch.AutoSize = true;
+            this.lblSearch.BackColor = System.Drawing.Color.Transparent;
+            this.lblSearch.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblSearch.Location = new System.Drawing.Point(424, 222);
+            this.lblSearch.Name = "lblSearch";
+            this.lblSearch.Size = new System.Drawing.Size(83, 22);
+            this.lblSearch.TabIndex = 18;
+            this.lblSearch.Text = "Search:";
+            // 
+            // textBoxSearch
+            // 
+            this.textBoxSearch.Location = new System.Drawing.Point(513, 224);
+            this.textBoxSearch.Name = "textBoxSearch";
+            this.textBoxSearch.Size = new System.Drawing.Size(379, 20);
+            this.textBoxSearch.TabIndex = 19;
+            this.textBoxSearch.TextChanged += new System.EventHandler(this.textBoxSearch_TextChanged);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(911, 2);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(38, 39);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 20;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // Econtact
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(951, 562);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.textBoxSearch);
+            this.Controls.Add(this.lblSearch);
+            this.Controls.Add(this.dgvContactList);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBoxGender);
+            this.Controls.Add(this.textAddress);
+            this.Controls.Add(this.lblAddress);
+            this.Controls.Add(this.textContactNo);
+            this.Controls.Add(this.textLastName);
+            this.Controls.Add(this.textFirstName);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblGender);
+            this.Controls.Add(this.lblContactNo);
+            this.Controls.Add(this.lblLastName);
+            this.Controls.Add(this.lblFirstName);
+            this.Controls.Add(this.lblIDContact);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Econtact";
+            this.Text = "Econtact";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvContactList)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblIDContact;
+        private System.Windows.Forms.Label lblFirstName;
+        private System.Windows.Forms.Label lblLastName;
+        private System.Windows.Forms.Label lblContactNo;
+        private System.Windows.Forms.Label lblGender;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textFirstName;
+        private System.Windows.Forms.TextBox textLastName;
+        private System.Windows.Forms.TextBox textContactNo;
+        private System.Windows.Forms.Label lblAddress;
+        private System.Windows.Forms.TextBox textAddress;
+        private System.Windows.Forms.ComboBox comboBoxGender;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.DataGridView dgvContactList;
+        private System.Windows.Forms.Label lblSearch;
+        private System.Windows.Forms.TextBox textBoxSearch;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ EContact/Econtact.cs
@@ -0,0 +1,181 @@
+﻿using EContact.econtactClasses;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EContact
+{
+    public partial class Econtact : Form
+    {
+        public Econtact()
+        {
+            InitializeComponent();
+        }
+
+        Class1 c = new Class1();
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DataTable dt = c.Select();
+            dgvContactList.DataSource = dt;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblIDContact_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        static string myconnstr = ConfigurationManager.ConnectionStrings["connstrng"].ConnectionString;
+        private void textBoxSearch_TextChanged(object sender, EventArgs e)
+        {
+            string keyword = textBoxSearch.Text;
+            SqlConnection conn = new SqlConnection(myconnstr);
+            SqlDataAdapter sda = new SqlDataAdapter("SELECT * FROM tbl_contact WHERE FirstName LIKE '%"+keyword+"%' OR LastName LIKE '%"+keyword+"%' OR Address LIKE '%"+keyword+"%'", conn);
+            DataTable dt = new DataTable(); 
+            sda.Fill(dt);
+            dgvContactList.DataSource = dt;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            c.FirstName = textFirstName.Text;
+            c.LastName = textLastName.Text;
+            c.ContactNo = textContactNo.Text;
+            c.Address = textAddress.Text;
+            c.Gender = comboBoxGender.Text;
+
+            bool success = c.Insert(c);
+            if (success == true)
+            {
+                MessageBox.Show("New Contact Successfully Insert");
+                Clear();
+            }
+            else
+            {
+                MessageBox.Show("Failed To Insert New Contact");
+            }
+
+            DataTable dt = c.Select();
+            dgvContactList.DataSource = dt;
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        public void Clear()
+        {
+            textFirstName.Text = "";
+            textLastName.Text = "";
+            textAddress.Text = "";
+            textContactNo.Text = "";
+            comboBoxGender.Text = "";
+            textBox1.Text = "";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            c.ContactID = int.Parse(textBox1.Text);
+            c.FirstName = textFirstName.Text;
+            c.LastName = textLastName.Text;
+            c.ContactNo= textContactNo.Text;
+            c.Address = textAddress.Text;
+            c.Gender = comboBoxGender.Text;
+
+            bool success = c.Update(c);
+            if(success==true)
+            {
+                MessageBox.Show("Contact Has Been Updated.");
+                DataTable dt = c.Select();
+                dgvContactList.DataSource = dt;
+                Clear();
+            }
+            else
+            {
+                MessageBox.Show("Contact Has Not Been Updated. Try Again.");
+            }
+        }
+
+        private void dgvContactList_RowHeaderMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            int rowIndex = e.RowIndex;
+            textBox1.Text = dgvContactList.Rows[rowIndex].Cells[0].Value.ToString();
+            textFirstName.Text = dgvContactList.Rows[rowIndex].Cells[1].Value.ToString();
+            textLastName.Text = dgvContactList.Rows[rowIndex].Cells[2].Value.ToString();
+            textContactNo.Text = dgvContactList.Rows[rowIndex].Cells[3].Value.ToString();
+            textAddress.Text = dgvContactList.Rows[rowIndex].Cells[4].Value.ToString();
+            comboBoxGender.Text = dgvContactList.Rows[rowIndex].Cells[5].Value.ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Clear();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            c.ContactID= Convert.ToInt32(textBox1.Text);
+            bool success = c.Delete(c);
+            if (success==true)
+            {
+                MessageBox.Show("Contact Has Been Successfully Deleted.");
+                DataTable dt = c.Select();
+                dgvContactList.DataSource = dt;
+                Clear();
+            }
+            else
+            {
+                MessageBox.Show("Contact Has Not Been Deleted. Try Again.");
+            }
+        }
+    }
+}
